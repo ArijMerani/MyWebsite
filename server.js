3b0cf74054442f4da05e841d6013d0168d747b5a
ArijMerani <arijmerani@gmail.com>
2024-05-17 16:17:47 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/projects', (req, res) => {
+    res.sendFile(__dirname + '/public/projects.html');
+});
+
+app.get('/education', (req, res) => {
+    res.sendFile(__dirname + '/public/education.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
